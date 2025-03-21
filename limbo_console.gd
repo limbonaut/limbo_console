09ece5c4faf61ef42a574e8de927ec54310a8ce9
Ryan <rcgilio@gmail.com>
2025-03-20 22:29:06 -0700

    Add tab completion to history search

--- limbo_console.gd
+++ limbo_console.gd
@@ -129,6 +129,9 @@ func handle_history_input(p_event: InputEvent):
 		_history_gui.increment_index()
 		_entry.grab_focus()
 		get_viewport().set_input_as_handled()
+	elif p_event.keycode == KEY_TAB and p_event.is_pressed():
+		_autocomplete()
+		get_viewport().set_input_as_handled()
 	elif p_event.keycode == KEY_DOWN and p_event.is_pressed():
 		_history_gui.decrement_index()
 		_entry.grab_focus()
