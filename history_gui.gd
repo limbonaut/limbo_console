2051756293f691507e684534bc2ae1b9c43c4223
Ryan <rcgilio@gmail.com>
2025-03-26 20:02:31 -0700

    Lint code

--- history_gui.gd
+++ history_gui.gd
@@ -6,26 +6,27 @@ extends Panel
 
 # Visual Elements
 const THEME_DEFAULT := "res://addons/limbo_console/res/default_theme.tres"
-var _last_highlighted_label : Label
-var _history_labels         : Array[Label]
-var _scroll_bar             : VScrollBar
-var _scroll_bar_width       = 12
+var _last_highlighted_label: Label
+var _history_labels: Array[Label]
+var _scroll_bar: VScrollBar
+var _scroll_bar_width = 12
 
 # Indexing Results
-var _command = "<placeholder>" # Needs default value so first search always processes
-var _command_history : Array # Command history to search throgh
-var _filter_results  : Array # Most recent results of performing a search for the _command in _command_history
+var _command = "<placeholder>"  # Needs default value so first search always processes
+var _command_history: Array  # Command history to search throgh
+var _filter_results: Array  # Most recent results of performing a search for the _command in _command_history
 
-var _display_count = 0 # Number of history items to display in search
-var _offset        = 0 # The offset _filter_results
-var _sub_index     = 0 # The highlight index 
+var _display_count = 0  # Number of history items to display in search
+var _offset = 0  # The offset _filter_results
+var _sub_index = 0  # The highlight index
 
 # Theme Colors [TODO: Flesh out theme colors]
-var _highlight_color : Color
+var _highlight_color: Color
 
 ################################################################################
 # Public Functions
 ################################################################################
+
 
 ## Set visibility of history search
 func set_visibility(p_visible):
@@ -36,15 +37,18 @@ func set_visibility(p_visible):
 		_update_scroll_list()
 	visible = p_visible
 
+
 ## Set the command history to search through
-func set_command_history(commands : Array):
+func set_command_history(commands: Array):
 	_command_history = commands
 	_update_highlight()
+
 
 ## Add a command to the history to search through
 func add_command(command):
 	_command_history.append(command)
 	_update_highlight()
+
 
 ## Move cursor downwards
 func decrement_index():
@@ -59,18 +63,20 @@ func decrement_index():
 		_sub_index -= 1
 		_update_highlight()
 
+
 ## Move cursor upwards
 func increment_index():
 	var current_index = _get_current_index()
 	if current_index + 1 >= _filter_results.size():
 		return
-		
+
 	if _sub_index >= _display_count - 1:
 		_offset += 1
 		_update_scroll_list()
 	else:
 		_sub_index += 1
 		_update_highlight()
+
 
 ## Get the current selected text
 func get_current_text():
@@ -79,6 +85,7 @@ func get_current_text():
 		current_text = _filter_results[_get_current_index()]
 	return current_text
 
+
 ## Search for the command in the history
 func search(command):
 	# Don't process if we used the same command before
@@ -86,30 +93,31 @@ func search(command):
 		return
 	_command = command
 
-	# Empty string so show all results	
+	# Empty string so show all results
 	if _command.length() == 0:
 		_filter_results = _command_history
-		
+
 		# Results are reversed since the list needs to go up instead of down
 		_filter_results.reverse()
 	else:
 		_filter_results = _fuzzy_match(command, _command_history)
-	
+
 	_reset_indexes()
 	_update_scroll_list()
 	_update_highlight()
+
 
 ################################################################################
 # Private Functions
 ################################################################################
 
+
 func _init() -> void:
-	set_anchors_preset(Control.PRESET_FULL_RECT) 
+	set_anchors_preset(Control.PRESET_FULL_RECT)
 	size_flags_horizontal = Control.SIZE_EXPAND_FILL
 	size_flags_vertical = Control.SIZE_EXPAND_FILL
 	_init_theme()
-	
-	
+
 	# Create first label, this is gets set to placeholder text to determine
 	# the display size once this node is _ready()
 	var new_item = Label.new()
@@ -118,9 +126,10 @@ func _init() -> void:
 	new_item.text = "<Placeholder>"
 	add_child(new_item)
 	_history_labels.append(new_item)
-	
+
 	_scroll_bar = VScrollBar.new()
 	add_child(_scroll_bar)
+
 
 ## Update the text in the scroll list to match current offset and filtered results
 func _update_scroll_list():
@@ -130,13 +139,14 @@ func _update_scroll_list():
 
 		# Default empty
 		_history_labels[i].text = ""
-		
+
 		# Set non empty if in range
 		var index_in_range = filter_index < _filter_results.size()
 		if index_in_range:
 			_history_labels[i].text += _filter_results[filter_index]
-	
+
 	_update_scroll_bar()
+
 
 ## Highlight the subindex
 func _update_highlight():
@@ -150,10 +160,11 @@ func _update_highlight():
 
 	if _filter_results.size() <= 0:
 		return
-		
+
 	var highlight_index = _sub_index
 	_history_labels[highlight_index].add_theme_stylebox_override("normal", style)
 	_last_highlighted_label = _history_labels[highlight_index]
+
 
 ## Initialize the theme and color variables
 func _init_theme() -> void:
@@ -161,7 +172,10 @@ func _init_theme() -> void:
 	_loaded_theme = load(THEME_DEFAULT)
 
 	const CONSOLE_COLORS_THEME_TYPE := &"ConsoleColors"
-	_highlight_color = _loaded_theme.get_color(&"history_highlight_color", CONSOLE_COLORS_THEME_TYPE)
+	_highlight_color = _loaded_theme.get_color(
+		&"history_highlight_color", CONSOLE_COLORS_THEME_TYPE
+	)
+
 
 ## Fuzzy search function similar to fzf
 static func _fuzzy_match(query: String, items: Array) -> Array:
@@ -176,6 +190,7 @@ static func _fuzzy_match(query: String, items: Array) -> Array:
 	results.sort_custom(func(a, b): return a.score > b.score)
 
 	return results.map(func(entry): return entry.item)
+
 
 ## Scoring function for fuzzy matching
 static func _compute_match_score(query: String, target: String) -> int:
@@ -196,28 +211,33 @@ static func _compute_match_score(query: String, target: String) -> int:
 			if query_index == query.length():
 				break
 
- 	# Ensure full query matches
+	# Ensure full query matches
 	return score if query_index == query.length() else 0
+
 
 ## Get the current index of the selected item
 func _get_current_index():
 	return _offset + _sub_index
+
 
 ## Reset offset and sub_indexes to scroll list back to bottom
 func _reset_indexes():
 	_offset = 0
 	_sub_index = 0
 
+
 func _ready():
 	# The sizing of the labels is dependant on visiblity
 	connect("visibility_changed", _calculate_display_count)
 	_scroll_bar.connect("scrolling", _scroll_bar_scrolled)
+
 
 ## When the scrollbar has been scrolled (by mouse), scroll the list
 func _scroll_bar_scrolled():
 	_offset = _scroll_bar.max_value - _display_count - _scroll_bar.value
 	_update_highlight()
 	_update_scroll_list()
+
 
 func _input(event):
 	# Scroll up/down on mouse wheel up/down
@@ -230,7 +250,7 @@ func _input(event):
 	# Remaining inputs are key press handles
 	if event is not InputEventKey:
 		return
-		
+
 	# Increment/Decrement index
 	if event.keycode == KEY_UP and event.is_pressed():
 		increment_index()
@@ -238,27 +258,27 @@ func _input(event):
 	elif event.keycode == KEY_DOWN and event.is_pressed():
 		decrement_index()
 		get_viewport().set_input_as_handled()
-	
+
 
 func _calculate_display_count():
 	if not visible:
 		return
 	# The display count is finnicky to get right due to the label needing to be
-	# rendered so the fize can be determined. This gets the job done, it ain't 
+	# rendered so the fize can be determined. This gets the job done, it ain't
 	# pretty, but it works
 	var max_y = size.y
-	
+
 	var label_size_y = (_history_labels[0] as Control).size.y
 	var label_size_x = size.x - _scroll_bar_width
-	
-	var display_count = (max_y as int / label_size_y as int)
+
+	var display_count = max_y as int / label_size_y as int
 	if _display_count != display_count and display_count != 0 and display_count > _display_count:
 		_display_count = (display_count as int)
-	
+
 	# Update first label and then the remaining new labels
 	_history_labels[0].position.y = size.y - label_size_y
 	_history_labels[0].set_size(Vector2(label_size_x, label_size_y))
-	for i in range(0, _display_count -_history_labels.size()):
+	for i in range(0, _display_count - _history_labels.size()):
 		var new_item = Label.new()
 		new_item.size_flags_vertical = Control.SIZE_SHRINK_END
 		new_item.size_flags_horizontal = Control.SIZE_EXPAND_FILL
@@ -266,18 +286,19 @@ func _calculate_display_count():
 		new_item.set_size(Vector2(label_size_x, label_size_y))
 		_history_labels.append(new_item)
 		add_child(new_item)
-	
+
 	# Update the scroll bar to be positioned correctly
 	_scroll_bar.size.x = _scroll_bar_width
 	_scroll_bar.size.y = size.y
 	_scroll_bar.position.x = label_size_x
-	
+
 	_reset_indexes()
 	_update_highlight()
 	_update_scroll_list()
 
+
 func _update_scroll_bar():
-	if (_filter_results.size() > 0 and _display_count > 0):
+	if (_filter_results.size() > 0) and (_display_count > 0):
 		var max_size = _filter_results.size()
 		_scroll_bar.max_value = max_size
 		_scroll_bar.page = _display_count
