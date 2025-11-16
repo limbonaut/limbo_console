44b7aafb1a4cdc291176af5f0bc6d15f88931cbc
Robert Paciorek <robert@opcode.eu.org>
2025-11-16 10:39:16 +0000

    add bash-like autocomplete mode
    * Add autocomplete with list of suggestions mode (default Ctrl+Tab):
       * If the complement is unambiguous, then a single keystroke causes the complement.
       * If the complement is ambiguous, then a single keystroke does nothing; the next keystroke displays a list of suggestions.
    * Use `is_action_pressed` with `exact_match=true` for all autocomplete actions
       * This allow the user to remap this mode to Tab and cycle mode to Ctrl+Tab
    * Add `print_in_columns` function for printing list of suggestions
       * It print single line, but due to RichTextLabel this ensures the correct column arrangement even when changing the window size.
    * Add `_add_input_action` in `plugin.gd` to remove code duplication.

--- plugin.gd
+++ plugin.gd
@@ -4,52 +4,38 @@ extends EditorPlugin
 const ConsoleOptions := preload("res://addons/limbo_console/console_options.gd")
 const ConfigMapper := preload("res://addons/limbo_console/config_mapper.gd")
 
+func _add_input_action(name : String, keycode : Key, shift := false, ctrl := false) -> int:
+	if not ProjectSettings.has_setting("input/" + name):
+		print("LimboConsole: Adding \"" + name + "\" input action to project settings...")
+
+		var key_event := InputEventKey.new()
+		key_event.keycode = keycode
+		key_event.shift_pressed = shift
+		key_event.ctrl_pressed = ctrl
+
+		ProjectSettings.set_setting("input/" + name, {
+			"deadzone": 0.5,
+			"events": [key_event],
+		})
+		return 1
+	return 0
+
 func _enable_plugin() -> void:
 	add_autoload_singleton("LimboConsole", "res://addons/limbo_console/limbo_console.gd")
 
 	# Sync config file (create if not exists)
 	var console_options := ConsoleOptions.new()
-	var do_project_setting_save: bool = false
 	ConfigMapper.load_from_config(console_options)
 	ConfigMapper.save_to_config(console_options)
 
-	if not ProjectSettings.has_setting("input/limbo_console_toggle"):
-		print("LimboConsole: Adding \"limbo_console_toggle\" input action to project settings...")
+	var created_actions: int = 0
+	created_actions += _add_input_action("limbo_console_toggle", KEY_QUOTELEFT)
+	created_actions += _add_input_action("limbo_auto_complete_forward", KEY_TAB, false, false)
+	created_actions += _add_input_action("limbo_auto_complete_reverse", KEY_TAB, true, false)
+	created_actions += _add_input_action("limbo_auto_complete_with_list", KEY_TAB, false, true)
+	created_actions += _add_input_action("limbo_console_search_history", KEY_R)
 
-		var key_event := InputEventKey.new()
-		key_event.keycode = KEY_QUOTELEFT
-
-		ProjectSettings.set_setting("input/limbo_console_toggle", {
-			"deadzone": 0.5,
-			"events": [key_event],
-		})
-		do_project_setting_save = true
-		
-	if not ProjectSettings.has_setting("input/limbo_auto_complete_reverse"):
-		print("LimboConsole: Adding \"limbo_auto_complete_reverse\" input action to project settings...")
-		var key_event = InputEventKey.new()
-		key_event.keycode = KEY_TAB
-		key_event.shift_pressed = true
-		
-		ProjectSettings.set_setting("input/limbo_auto_complete_reverse", {
-			"deadzone": 0.5,
-			"events": [key_event],
-		})
-		do_project_setting_save = true
-	
-	if not ProjectSettings.has_setting("input/limbo_console_search_history"):
-		print("LimboConsole: Adding \"limbo_console_search_history\" input action to project settings...")
-		var key_event = InputEventKey.new()
-		key_event.keycode = KEY_R
-		key_event.ctrl_pressed = true
-		
-		ProjectSettings.set_setting("input/limbo_console_search_history", {
-			"deadzone": 0.5,
-			"events": [key_event],
-		})
-		do_project_setting_save = true
-		
-	if do_project_setting_save:
+	if created_actions > 0:
 		ProjectSettings.save()
 
 
